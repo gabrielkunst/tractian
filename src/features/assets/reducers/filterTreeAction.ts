d64e1c2d0ebd9fa77a28ba44fc25241b98e49169
Gabriel Kunst <gabrielkt.dev@gmail.com>
2024-08-19 22:48:01 -0300

    feat: add filter nodes function

--- src/features/assets/reducers/filterTreeAction.ts
+++ src/features/assets/reducers/filterTreeAction.ts
@@ -6,11 +6,61 @@ export function filterTreeAction(
   search: string,
   filters: TreeFilter
 ): TreeNode[] {
-  console.log('FILTERING TREE: ', {
-    tree,
-    search,
-    filters,
-  })
+  const searchLowerCase = search.toLowerCase().trim()
+  const hasActiveFilters = hasFilters(filters)
 
   return tree
+    .map((node) => filterNode(node, searchLowerCase, filters, hasActiveFilters))
+    .filter((node): node is TreeNode => node !== null)
+}
+
+function filterNode(
+  node: TreeNode,
+  searchLowerCase: string,
+  filters: TreeFilter,
+  hasActiveFilters: boolean
+): TreeNode | null {
+  const { name, children } = node
+  const nameLowerCase = name.toLowerCase()
+
+  const doesNameMatch =
+    !searchLowerCase || nameLowerCase.includes(searchLowerCase)
+  const doesFilterMatch = applyFilters(node, filters)
+
+  const filteredChildren = children
+    ? filterTreeAction(children, searchLowerCase, filters)
+    : []
+
+  const isExpanded =
+    hasActiveFilters &&
+    ((doesNameMatch && doesFilterMatch) || filteredChildren.length > 0)
+
+  if ((doesNameMatch && doesFilterMatch) || filteredChildren.length > 0) {
+    return {
+      ...node,
+      isExpanded,
+      children: filteredChildren.length > 0 ? filteredChildren : children || [],
+    }
+  }
+
+  return null
+}
+
+function applyFilters(
+  { status, sensorType }: TreeNode,
+  { criticalStatus, energySensor }: TreeFilter
+): boolean {
+  if (criticalStatus && status !== 'alert') {
+    return false
+  }
+
+  if (energySensor && sensorType !== 'energy') {
+    return false
+  }
+
+  return true
+}
+
+function hasFilters(filters: TreeFilter): boolean {
+  return Object.values(filters).some(Boolean)
 }
